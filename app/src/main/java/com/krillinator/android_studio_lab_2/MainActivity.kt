8488b117a3517dabeb4463ef1e413956dc9f77df
nasvalljohan <j.nasvall95@gmail.com>
2022-03-15 11:58:43 +0100

    Initial commit

---
+++ app/src/main/java/com/krillinator/android_studio_lab_2/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.krillinator.android_studio_lab_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/* TODO READ THE README FILE FIRST */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val loginBtn = findViewById<Button>(R.id.textView)
+
+        TODO("Create Intent to navigate")
+    }
+}
---
+++ app/src/main/java/com/krillinator/android_studio_lab_2/UserViewModel.kt
@@ -0,0 +1,9 @@
+package com.krillinator.android_studio_lab_2
+
+class UserViewModel {
+
+    /* TODO -
+    *       Implement USER
+    *       Extend to ViewModel (SOLO GROUPS ONLY)
+    * */
+}
