8dffbe31ae8e0078e161780f0bdc65d53dc56ca4
Ploo Jompong <50630228+ponjom2@users.noreply.github.com>
2022-03-23 09:15:41 +0100

    Update LoginActivity.kt

--- app/src/main/java/com/ploohan/android_studio_lab_2/LoginActivity.kt
+++ app/src/main/java/com/ploohan/android_studio_lab_2/LoginActivity.kt
@@ -19,9 +19,6 @@ class LoginActivity : AppCompatActivity() {
         // Sent emailtext to mainactivity with intent, added to textview.
         // Arraylist for email & password, checking if pw & email matches.
 
-
-        // Tjena PLOOOOOO
-
         val emailText = findViewById<EditText>(R.id.EmailText)
         val passWordText = findViewById<EditText>(R.id.pwText)
         val loginBtn = findViewById<Button>(R.id.loginBtn)
